3ed81e01425bf04d26d0318a2fcc9cd7d48dd0c3
Edofo <48387251+Edofo@users.noreply.github.com>
2025-03-04 14:39:06 +0100

    feat: create identifyHand & compareHands function

---
+++ src/poker.ts
@@ -0,0 +1,7 @@
+export const identifyHand = (hand: string[]) => {
+  throw new Error('Not implemented');
+};
+
+export const compareHands = (hand1: string[], hand2: string[]) => {
+  throw new Error('Not implemented');
+};
